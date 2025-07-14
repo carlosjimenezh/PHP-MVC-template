2c03aa8498e774d53d681c9cdf3c19c210396b9b
carlosejimenezh95 <carlosejimenezh95@gmail.com>
2025-07-14 10:56:19 -0600

    asegurar la respuesta en utf8 de base de datos

--- api/db/connection.php
+++ api/db/connection.php
@@ -2,11 +2,13 @@
 function conn()
 {
 	$mysqli=new mysqli (DB_HOST, DB_ADMIN_USER, DB_ADMIN_PASS, DB_NAME);
+	  $mysqli->query("SET NAMES 'utf8'");
 	return $mysqli;
 }
 function lectura()
 {
 	$mysqli=new mysqli (DB_HOST, DB_USER, DB_PASS, DB_NAME);
+	  $mysqli->query("SET NAMES 'utf8'");
 	return $mysqli;
 }
 function desconectar($mysqli){
